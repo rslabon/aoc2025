99cd10ea185528b3913b5e91a895e81a0215b907
abc <abc@abc.abc>
2026-02-28 19:20:35 +0100

    day 1 - done

--- day1.py
+++ day1.py
@@ -1,8 +1,8 @@
 with open("./resources/day1.txt") as f:
-    data = f.read().strip()
+    data = f.read()
 
 rotations = []
-for line in data.split("\n"):
+for line in data.strip().split("\n"):
     if line.startswith("L"):
         rotations.append(-int(line.replace("L", "")))
     else:
@@ -14,7 +14,7 @@ def part1():
     zero_count = 0
     for rotation in rotations:
         dial += rotation
-        if dial < 0 or dial > 90:
+        if dial < 0 or dial > 99:
             dial %= 100
 
         if dial == 0:
@@ -23,4 +23,38 @@ def part1():
     print(zero_count)
 
 
+def rotate(dial, rotation):
+    zero_count = 0
+    if rotation > 0:
+        while rotation > 0:
+            dial += 1
+            if dial == 100:
+                dial = 0
+                zero_count += 1
+
+            rotation -= 1
+    elif rotation < 0:
+        while rotation < 0:
+            dial -= 1
+            if dial == 0:
+                zero_count += 1
+            if dial == -1:
+                dial = 99
+
+            rotation += 1
+
+    return dial, zero_count
+
+
+def part2():
+    dial = 50
+    zero_count = 0
+    for rotation in rotations:
+        dial, zero = rotate(dial, rotation)
+        zero_count += zero
+
+    print(zero_count)
+
+
 part1()
+part2()
