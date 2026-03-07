c78bcaa56d17432650de8c4aa88674ecbc769fd1
abc <abc@abc.abc>
2026-03-07 16:28:10 +0100

    day 5 - done

--- day5.py
+++ day5.py
@@ -20,7 +20,7 @@ ranges = []
 for line in part1.strip().split("\n"):
     start, end = line.split("-")
     start, end = int(start), int(end)
-    ranges.append((start, end))
+    ranges.append([start, end])
 
 ranges = list(sorted(ranges, key=lambda x: (x[0], x[1])))
 
@@ -38,4 +38,30 @@ def part1():
     print(fresh)
 
 
+def intersects(r1, r2):
+    start_r1, end_r1 = r1
+    start_r2, end_r2 = r2
+    return start_r2 <= end_r1
+
+
+def merge_ranges(ranges):
+    result = [ranges[0]]
+    for i in range(1, len(ranges)):
+        if intersects(result[-1], ranges[i]):
+            result[-1][1] = max(ranges[i][1], result[-1][1])
+        else:
+            result.append(ranges[i])
+
+    return result
+
+
+def part2():
+    fresh = 0
+    for (start, end) in merge_ranges(ranges):
+        fresh += end - start + 1
+
+    print(fresh)
+
+
 part1()
+part2()
